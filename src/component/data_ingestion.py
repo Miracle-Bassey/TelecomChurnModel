f8f047fe5f2d2c49597c2f3d05a0fef0a6379e88
Miracle <basseymiracleosa@gmail.com>
2025-01-29 16:48:08 -0600

    Data Injestion

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Initiating data ingestion")
         try:
-            df=pd.read_csv(notebook/data/customer_churn.csv)
+            df=pd.read_csv("notebook/data/customer_churn.csv")
             logging.info("Read data from csv to dataframe")
 
             # create data directories
@@ -34,10 +34,32 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            # create test split
+            # perform stratified train-test split
             logging.info("Train test split initiated")
+            target_column ='Churn'
+            train_set, test_set = train_test_split(
+            df,
+            test_size=0.2,
+            random_state=42,
+            stratify=df[target_column]
+            )
+            # save train and test sets to their respective directories
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data ingestion completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
 
         except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
 
 
 
